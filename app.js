c6b46f81c71f15a9e253b9344d86bc19cc6f409a
BUBU <cris.cadosh@gmail.com>
2021-10-12 07:33:35 -0400

    Incement Counter

---
+++ app.js
@@ -0,0 +1,23 @@
+const counters = document.querySelectorAll('.counter');
+
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+
+    const upadtaCounter = () => {
+        const target =+ counter.getAttribute('data-target')
+        const   c =+ counter.innerText
+
+        const increment = target / 200;
+
+        if(c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(upadtaCounter, 1)
+        }else{
+            counter.innerText = target
+        }
+    }
+
+    upadtaCounter()
+})
